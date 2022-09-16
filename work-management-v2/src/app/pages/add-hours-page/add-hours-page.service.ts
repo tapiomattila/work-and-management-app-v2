3456c3889052434d0542fc598709a3df4c429e83
Tapio Mattila <tapiomattila193@gmail.com>
2022-09-16 20:47:51 +0300

    added add hours page service

---
+++ work-management-v2/src/app/pages/add-hours-page/add-hours-page.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class AddHoursPageService {
+    constructor() { }
+
+    // day marked hours
+
+    // date selection
+
+    // dropdown selection + slider value
+
+    // marked hours list and selection toggle
+
+}
